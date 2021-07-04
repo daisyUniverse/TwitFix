5ac4879f021043ac3c8490dfe657d8c504c004d8
Conner Bondurant <conner.bondurant@triptych.me>
2021-07-03 21:41:47 -0400

    Improve handling and allow for exclusion of hostname
    This allows for the removal of "https://twitter.com/"
    This could be more convinent as you simply replace "twitter.com" with
    twtfix.me in the url, removing having to type http:// each time
    
    also added additional error handling just to avoid messy 500 errors.

--- twitfix.py
+++ twitfix.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
 import youtube_dl
-import requests
-import psutil
 import os
+import re
 
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 app = Flask(__name__)
+pathregex = re.compile("\\w{1,15}\\/status\\/\\d{19}")
 
 @app.route('/<path:subpath>')
 def twitfix(subpath):
-    if subpath.startswith('https://twitter.com'):
-        with ydl:
-            result = ydl.extract_info(subpath, download=False)
 
-        return render_template('index.html', vidurl=result['url'], tweet=result['description'], pic=result['thumbnail'], user=result['uploader'], tweeturl=subpath)
+    match = pathregex.search(subpath)
+    if match is not None:
+        twitter_url = subpath
+
+        if match.start() == 0:
+            twitter_url = "https://twitter.com/" + subpath
+
+        with ydl:
+            try:
+                result = ydl.extract_info(twitter_url, download=False)
+            except Exception: # Just to keep from 500s that are messy
+                return "Bad twitter link, try again"
+
+        return render_template('index.html', vidurl=result['url'], tweet=result['description'], pic=result['thumbnail'], user=result['uploader'], tweeturl=twitter_url)
     else:
         return "Please use a twitter link"
 
